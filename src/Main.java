578fbc381133861f71a776ec5f4d3e31adc254a3
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-27 14:03:09 +0100

    Feature: moltiplicazione, potenza, pari o dispari

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import  java.lang.Math;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(" Second Commit");
+    }
+
+    public static double moltiplicazione(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double potenza(double base, double esponente) {
+        return Math.pow(base, esponente);
+    }
+
+    //Se il numero è pari ritorna true, altrimenti false
+    public static boolean oddOrEven(double num) {
+        double odd = num % 2;
+        return odd == 0;
     }
 
 }
