c7916f95b9d68884ee2ab97b0c3f372c766dcc99
Giovanni Battista Innaimi <giovanni.innaimi@protonmail.com>
2024-02-27 15:39:40 +0100

    Feat. adjusting: funzioni precedentemente dichiarati, settati senza alcun valore di ritorno

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,23 @@ public class Main {
     public static void main(String[] args) {
     }
 
-    public static double moltiplicazione(double num1, double num2) {
-        return num1 * num2;
+    public static void moltiplicazione(double num1, double num2) {
+        System.out.println("Il risultato della moltiplicazione è: " + num1 * num2);
     }
 
-    public static double potenza(double base, double esponente) {
-        return Math.pow(base, esponente);
+    public static void potenza(double base, double esponente) {
+        System.out.println("Il risultato della potenza è: " + Math.pow(base, esponente));
     }
 
     //Se il numero è pari ritorna true, altrimenti false
-    public static boolean oddOrEven(double num) {
+    public static void oddOrEven(double num) {
         double odd = num % 2;
-        return odd == 0;
+        if(odd == 0) {
+            System.out.println("Il inserito numero è pari!");
+        }
+        else {
+            System.out.println("Il inserito numero è dispari!");
+        }
     }
 
 }
